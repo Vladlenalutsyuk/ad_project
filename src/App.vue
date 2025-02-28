86cb0e032fd3e72ea796f525c19b6fd3cafc3496
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-02-28 09:40:25 +0300

    Добавление иконки к кнопке

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
   <v-app>
-  <v-navigation-drawer app v-model="drawer"></v-navigation-drawer> 
+  <v-navigation-drawer app></v-navigation-drawer>
   <v-app-bar app dark color="primary">
-    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-    <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-sm-and-down">
-       <v-btn>
-<v-icon
-start
-icon="mdi-cake-variant"
-></v-icon>
-Link One
-</v-btn>
-       <v-btn>Link Two</v-btn> 
-       <v-btn>Link Three</v-btn>
-    </v-toolbar-items>
+  <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  <v-spacer></v-spacer>
+  <v-toolbar-items class="hidden-sm-and-down">
+  <v-btn>
+  <v-icon
+  start
+  icon="mdi-cake-variant"
+  ></v-icon>
+  Link One
+  </v-btn>
+  <v-btn>Link Two</v-btn>
+  <v-btn>Link Three</v-btn>
+  </v-toolbar-items>
   </v-app-bar>
-  <v-content>
-  </v-content>
+  <v-main>
+  </v-main>
   </v-app>
-</template>
-
+  </template>
+  
+  
 <script>
 export default {
   data() {
