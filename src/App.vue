8311654b60b8cb63975d5e60f88b4b1fa207c3ab
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-03-13 11:16:05 +0300

    Создание динамического меню

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,8 @@
   </v-btn>
   </v-toolbar-items>
   </v-app-bar>
-  <v-main>
+  <v-main> 
+    <router-view></router-view>
   </v-main>
   </v-app>
   </template>
@@ -65,7 +66,7 @@
   }
   }
   </script>
-  
+
 
 
   
